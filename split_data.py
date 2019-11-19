d9311d1d74a92a594b5adcc07526d1232323c365
EmmyFang <emmyfang.c@gmail.com>
2019-11-18 22:45:24 -0500

    clean up split_data.py

--- split_data.py
+++ split_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from scipy.signal import savgol_filter
+
 
 import torch
 """
@@ -25,66 +25,22 @@ text = 'text'
 label = 'label'
 fpath = './data/data.tsv'
 data = pd.read_table(fpath, header = 0)
-num = data['label'].value_counts()[0]
-data1, data0 = np.split(data, [num],axis = 0)
-data1.columns=['text','label']
-data0.columns=['text','label']
+# data['label'].dtype('int')
+print('total: subjective: {}, objective: {}'.format(data['label'].value_counts()[0], data['label'].value_counts()[1]))
 
-data1_text, data1_label = np.split(data1, [1], axis = 1)
-data0_text, data0_label = np.split(data0, [1], axis = 1)
+temp, test = train_test_split(data, test_size=0.2, random_state=seed, stratify=data['label'])
 
-text1_train, text1_left, label1_train, label1_left = train_test_split (data1_text, data1_label, test_size = 1-train_portion, random_state=seed)
-text0_train, text0_left, label0_train, label0_left = train_test_split (data0_text, data0_label, test_size = 1-train_portion, random_state=seed)
-
-
-text_train = [text1_train, text0_train]
-text_train = pd.concat(text_train)
-
-label_train = [label1_train, label0_train]
-label_train = pd.concat(label_train)
-
-
-
-text1_val, text1_test, label1_val, label1_test = train_test_split (text1_left, label1_left, test_size = test_portion/(1-train_portion), random_state=seed)
-text0_val, text0_test, label0_val, label0_test = train_test_split (text0_left, label0_left, test_size = test_portion/(1-train_portion), random_state=seed)
-
-text_val = [text1_val, text0_val]
-text_val = pd.concat(text_val)
-
-label_val = [label1_val, label0_val]
-label_val = pd.concat(label_val)
-
-text_test = [text1_test,text0_test]
-text_test = pd.concat(text_test)
-
-label_test = [label1_test, label0_test]
-label_test = pd.concat(label_test)
-
-text_test = text_test.values.squeeze()
-label_test = label_test.values.squeeze()
-test = pd.DataFrame({'text': text_test, 'label': label_test })
-test.to_csv('./data/test.tsv', sep='\t', index = False)
-
-text_val = text_val.values.squeeze()
-label_val = label_val.values.squeeze()
-val = pd.DataFrame({'text': text_val, 'label': label_val})
-val.to_csv('./data/validation.tsv', sep='\t', index = False)
-
-
-text_train = text_train.values.squeeze()
-label_train = label_train.values.squeeze()
-train = pd.DataFrame({'text': text_train, 'label': label_train})
-train.to_csv('./data/train.tsv', sep='\t', index = False)
-
+train, val = train_test_split(temp, test_size=val_portion/(1-test_portion), random_state=seed, stratify=temp['label'])
 
 print('train: {}'.format(train.shape))
-print(train['label'].value_counts())
-print()
+print(train['label'].value_counts(),'\n')
 
 print('validation: {}'.format(val.shape))
-print(val['label'].value_counts())
-print()
-
+print(val['label'].value_counts(), '\n')
 
 print('test: {}'.format(test.shape))
-print(test['label'].value_counts())
+print(test['label'].value_counts(),'\n')
+
+train.to_csv('./data/train.tsv', sep='\t', index = False)
+val.to_csv('./data/validation.tsv', sep='\t', index = False)
+test.to_csv('./data/test.tsv', sep='\t', index = False)
